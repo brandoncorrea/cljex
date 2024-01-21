8d588d54e7b458a673a28f69bce0fd70db8fa32b
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-01-21 12:19:54 -0600

    Magic squares and benchmarking

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.logic "1.0.1"]]
+                 [org.clojure/core.logic "1.0.1"]
+                 [org.clojure/math.combinatorics "0.2.0"]]
   :profiles {:dev {:dependencies [[speclj "3.4.3"]]}}
   :plugins [[speclj "3.4.3"]]
   :test-paths ["spec"])
--- spec/cljex/log/fun_spec.clj
+++ spec/cljex/log/fun_spec.clj
@@ -1,9 +1,172 @@
 (ns cljex.log.fun-spec
   "Nothing... yet"
-  (:require [clojure.core.logic :refer [== all and* conda conde conso fail fresh membero or* permuteo run run* succeed]]
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer [== all and* conda conde conso distincto fail fresh membero or* permuteo run run* succeed]]
+            [clojure.core.logic.fd :as fd]
+            [clojure.math.combinatorics :as combo]
             [speclj.core :refer :all]))
 
+(defmacro elapsed-time
+  "Evaluates the body and returns the elapsed time in milliseconds."
+  [& body]
+  `(let [start# (System/nanoTime)]
+     (do ~@body)
+     (/ (double (- (System/nanoTime) start#)) 1000000.0)))
+
+(defn reduce-times
+  "Reduces on a function n times. f is a single-arity function,
+   which accepts the result of its last invocation."
+  ([f n] (reduce-times f nil n))
+  ([f init n] (reduce (fn [acc _] (f acc)) init (range n))))
+
+(defmacro bench-n
+  "Test how long a body will take to execute.
+   This will only `(do body)`. If you need to realize
+   all results in a sequence, wrap your body in a `doall`.
+   Body is executed n times."
+  [n & body]
+  `(let [n# ~n]
+     (when (pos? n#)
+       (let [timings# (repeatedly n# #(elapsed-time ~@body))
+             total#   (apply + timings#)]
+         {:min   (apply min timings#)
+          :max   (apply max timings#)
+          :total total#
+          :avg   (/ total# n#)}))))
+
+(defmacro bench
+  "Same as (bench-n 10 ...)"
+  [& body] `(bench-n 10 ~@body))
+
+(defmacro sum
+  ([x y binding] `(fd/+ ~x ~y ~binding))
+  ([x y z binding]
+   `(fresh [s#]
+      (fd/+ ~x ~y s#)
+      (fd/+ s# ~z ~binding)))
+  ([w x y z binding]
+   `(fresh [s1# s2#]
+      (fd/+ ~w ~x s1#)
+      (fd/+ s1# ~y s2#)
+      (fd/+ s2# ~z ~binding))))
+
+(defmacro sum== [comp & vars]
+  `(fresh [s#]
+     (sum ~@vars s#)
+     (== s# ~comp)))
+
+(defn magic-constant
+  ([n] (magic-constant 1 n))
+  ([start n]
+   (/ (* n
+         (dec
+           (+ (* 2 start)
+              (* n n))))
+      2)))
+
+(defn magic-3x3-squares [lower-bound upper-bound]
+  (let [median         (- upper-bound (/ (- upper-bound lower-bound) 2))
+        magic-constant (magic-constant 3)]
+    (run* [q]
+      (fresh [a b c
+              d e f
+              g h i]
+
+        (distincto [a b c d e f g h i])
+        (fd/in a b c d e f g h i (fd/interval lower-bound upper-bound))
+
+        ;; Odd Squares will always have the median value in its center
+        (== e median)
+        (fresh [row-sum]
+          ;; All rows, columns, and main diagonals will sum to the magic constant.
+          (== row-sum magic-constant)
+          (sum a b c row-sum)
+          (sum== row-sum d e f)
+          (sum== row-sum g h i)
+          (sum== row-sum a d g)
+          (sum== row-sum b e h)
+          (sum== row-sum c f i)
+          (sum== row-sum a e i)
+          (sum== row-sum c e g))
+
+        (== q [a b c d e f g h i])))))
+
+(defn magic-4x4-squares [limit lower-bound upper-bound]
+  (let [magic-constant (magic-constant 3)]
+    (run limit [q]
+      (fresh [a b c d
+              e f g h
+              i j k l
+              m n o p]
+        (distincto [a b c d e f g h i j k l m n o p])
+        (fd/in a b c d e f g h i j k l m n o p (fd/interval lower-bound upper-bound))
+
+        (fresh [row-sum]
+          ;; All rows, columns, and main diagonals will sum to the magic constant.
+          (== row-sum magic-constant)
+          (sum a b c d row-sum)
+          (sum== row-sum e f g h)
+          (sum== row-sum i j k l)
+          (sum== row-sum m n o p)
+          (sum== row-sum a e i m)
+          (sum== row-sum b f j n)
+          (sum== row-sum c g k o)
+          (sum== row-sum d h l p)
+          (sum== row-sum a f k p)
+          (sum== row-sum m j g d))
+
+        (== q [a b c d e f g h i j k l m n o p])))))
+
+(defn indices-for-square [n]
+  (let [n-1           (dec n)
+        n+1           (inc n)
+        range-n       (vec (range n))
+        col-indices   (map (fn [c] (map #(+ (* n %) c) range-n)) range-n)
+        row-indices   (partition n (range (* n n)))
+        diag1-indices (map #(* % n+1) range-n)
+        diag2-indices (map #(* % n-1) (range 1 n+1))]
+    (mapv vec (concat col-indices row-indices [diag1-indices diag2-indices]))))
+
+(defn generate-magic-squares
+  ([n] (generate-magic-squares n 1))
+  ([n start]
+   (let [;; All rows, columns, and main diagonals will sum to the magic constant.
+         magic-constant (magic-constant start n)
+         magic-indices? (fn [coll indices] (= magic-constant (reduce #(+ %1 (nth coll %2)) 0 indices)))
+         indices        (indices-for-square n)
+         upper-bound    (+ start (* n n))
+         interval       (range start upper-bound)
+         median-index   (int (/ (* n n) 2))
+         median-val     (nth interval median-index)]
+     (->> (combo/permutations interval)
+          (filter
+            (cond->> #(every? (partial magic-indices? %) indices)
+                     ;; Odd Squares will always have the median value in its center
+                     (odd? n)
+                     (every-pred #(= median-val (nth % median-index)))))))))
+
 (describe "Logic Fun"
+
+  (it "filtering"
+    (let [vals (shuffle (range 1 101))
+          odds (shuffle (filter odd? vals))]
+      (should= (set (filter (fn [m] (some #(= % m) odds)) vals))
+               (set
+                 (run* [q]
+                   (membero q vals)
+                   (membero q odds))))))
+
+  (it "magic squares"
+    (should= 15 (magic-constant 3))
+    (should= 34 (magic-constant 4))
+    (should= 34 (magic-constant 1 4))
+    (should= 70 (magic-constant 10 4))
+    ;(should= 1 (bench-n 10 (doall (generate-magic-squares 3))))
+    (should= (set (generate-magic-squares 3))
+             (set (magic-3x3-squares 1 9)))
+    ;; WLL NOT TERMINATE
+    #_(should= (set (generate-magic-squares 4))
+               (set (magic-4x4-squares 1 1 16))))
 
   (it "takes n results"
     (let [odds [1 3 5 7 9]]
